65889fee495ec24571fd7f937abd74e893b13dd3
alexeylatypov <alexey.latypov@live.com>
2015-08-19 09:58:37 +0500

    updated index.php

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
 <?php
+error_reporting(E_ALL);
+class AsyncOperation extends Thread {
+  public function __construct($arg){
+    $this->arg = $arg;
+  }
+
+  public function run(){
+    if($this->arg){
+      printf("Hello %s\n", $this->arg);
+    }
+  }
+}
+$thread = new AsyncOperation("World");
+if($thread->start())
+  $thread->join();
+
 phpinfo();
 
 ?>
