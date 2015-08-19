42c4609a4cce186d7de23bbf4c770732f6bc20a6
alexeylatypov <alexey.latypov@live.com>
2015-08-19 21:52:58 +0500

    update index.php
    MonteCarlo function added

--- index.php
+++ index.php
@@ -9,13 +9,22 @@ function __autoload($class_name) {
 }
 
 
-$test = ["Hello", "World"];
-$closure = function($test) {
-    return $test;
+
+$closure = function MonteCarlo($n) {
+	$np=0;
+	for ($i=0; $i<=$n; $++){
+		$x=mt_rand(0,1)-1;
+		$y=mt_rand(0,1)-1;
+		if((pow($x,2)+pow($y,2))<=1) {
+			$np++;
+		}
+	}
+	$pi = 4 * $np / $n;
+    return $pi;
 };
 /* make call in background thread */
-$future = ParallelThread::add($closure, [$test]);
+$getpi = ParallelThread::add($closure, 10);
 /* get result of background and foreground call */
-var_dump($future->getResult(), $closure($test));
+var_dump($getpi->getResult(), $closure($test));
 
 ?>
