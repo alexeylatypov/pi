f927753e545f90ca48b1353294eb06588a16168b
alexeylatypov <alexey.latypov@live.com>
2015-08-20 01:31:28 +0500

    updated

--- index.php
+++ index.php
@@ -19,8 +19,8 @@ function __autoload($class_name) {
 $closure = function($n) {
 	$np=0;
 	for ($i=0; $i<=$n; $i++){
-		$x=rand(1,999999999999)/1000000000000*2-1;
-		$y=rand(1,999999999999)/1000000000000*2-1;
+		$x=rand(1,999999999999999)/1000000000000000*2-1;
+		$y=rand(1,999999999999999)/1000000000000000*2-1;
 		//var_dump($x+''+$y);
 		if((pow($x,2)+pow($y,2))<=1) {
 			$np++;
@@ -37,20 +37,20 @@ $argv = [1000000];
 $time_start = microtime(true);
 $np_result =0;
 // Initialize and start the threads
-foreach (range(0, 30) as $i) {
+foreach (range(0, 10) as $i) {
     $workers[$i] = new ParallelThread($closure, $argv );
 	$workers[$i]->start();
 }
  
 // Let the threads come back
-foreach (range(0, 30) as $i) {
+foreach (range(0, 10) as $i) {
     $workers[$i]->join();
 	var_dump($workers[$i]->getResult());
 	echo "<br>";
 	$tmp_res = json_decode($workers[$i]->getResult(), true);
 	$np_result = $np_result + $tmp_res['results'];
 }
-$pi = 4 * $np_result / ($argv[0]*31);
+$pi = 4 * $np_result / ($argv[0]*11);
 echo $np_result."<br>";
 echo $pi."<br>";
 echo microtime(true) - $time_start;
