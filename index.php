3bc5fcff3ea46e783921c5b49560ac1e6c8be3db
alexeylatypov <alexey.latypov@live.com>
2015-08-20 00:52:54 +0500

    updated

--- index.php
+++ index.php
@@ -32,18 +32,18 @@ $closure = function($n) {
 
 
 /* make call in background thread */
-$argv = [1000000];
+$argv = [10000000];
 
 $time_start = microtime(true);
 
 // Initialize and start the threads
-foreach (range(0, 10) as $i) {
+foreach (range(0, 1) as $i) {
     $workers[$i] = new ParallelThread($closure, $argv );
 	$workers[$i]->start();
 }
  
 // Let the threads come back
-foreach (range(0, 10) as $i) {
+foreach (range(0, 1) as $i) {
     $workers[$i]->join();
 	var_dump($workers[$i]->getResult());
 		echo "<br>";
