c1dd6ba6734aa1e1f51044de8f79ca73e0d2efc1
alexeylatypov <alexey.latypov@live.com>
2015-08-20 00:58:16 +0500

    updated

--- index.php
+++ index.php
@@ -19,8 +19,8 @@ function __autoload($class_name) {
 $closure = function($n) {
 	$np=0;
 	for ($i=0; $i<=$n; $i++){
-		$x=rand(1,999999999)/1000000000*2-1;
-		$y=rand(1,999999999)/1000000000*2-1;
+		$x=rand(1,999999999999)/1000000000000*2-1;
+		$y=rand(1,999999999999)/1000000000000*2-1;
 		//var_dump($x+''+$y);
 		if((pow($x,2)+pow($y,2))<=1) {
 			$np++;
@@ -32,18 +32,18 @@ $closure = function($n) {
 
 
 /* make call in background thread */
-$argv = [10000];
+$argv = [99999];
 
 $time_start = microtime(true);
 
 // Initialize and start the threads
-foreach (range(0, 1000) as $i) {
+foreach (range(0, 100) as $i) {
     $workers[$i] = new ParallelThread($closure, $argv );
 	$workers[$i]->start();
 }
  
 // Let the threads come back
-foreach (range(0, 1000) as $i) {
+foreach (range(0, 100) as $i) {
     $workers[$i]->join();
 	var_dump($workers[$i]->getResult());
 		echo "<br>";
