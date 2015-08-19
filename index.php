f2995ef65499a1c64fe26aaac86998d3c76b833e
alexeylatypov <alexey.latypov@live.com>
2015-08-19 21:55:27 +0500

    update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ function __autoload($class_name) {
 
 $closure = function($n) {
 	$np=0;
-	for ($i=0; $i<=$n; $++){
+	for ($i=0; $i<=$n; $i++){
 		$x=mt_rand(0,1)-1;
 		$y=mt_rand(0,1)-1;
 		if((pow($x,2)+pow($y,2))<=1) {
