cf0e4091ec00a0126de02d44c3c2ccf06dd99039
alexeylatypov <alexey.latypov@live.com>
2015-08-21 10:42:20 +0500

    updated

--- worker.php
+++ worker.php
@@ -7,7 +7,7 @@ function __autoload($class_name) {
       return false; 
 }
 
-$phpscrit="phpinfo();";
+$phpscrit="<?php phpinfo(); ?>";
 $cmd = Command::factory('php');
 $cmd->setCallback(function($pipe, $data){
         if ($pipe === Command::STDOUT) echo 'STDOUT: ';
