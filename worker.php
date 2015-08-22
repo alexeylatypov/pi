abded0194fe0c6b48795dd739c53a592568019e8
alexeylatypov <alexey.latypov@live.com>
2015-08-22 13:52:12 +0500

    updated

--- worker.php
+++ worker.php
@@ -17,8 +17,8 @@ $cmd->setCallback(function($pipe, $data){
         // return false;
     })
     ->setDirectory('/tmp')
-    ->option('$phpscrit')
-	->argument()
+    ->option('-r')
+	->argument($phpscrit)
     ->run();
 if ($cmd->getExitCode() === 0) {
     echo $cmd->getStdOut();
