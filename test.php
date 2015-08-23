b19f24b8cee4cb3e9d5e168c2f1650d6b705dc50
alexeylatypov <alexey.latypov@live.com>
2015-08-23 13:27:18 +0500

    Files added

---
+++ test.php
@@ -0,0 +1,12 @@
+<?php
+include_once("STUNClient.php");
+//屏蔽socket通信警告
+error_reporting(E_ERROR);
+$sc = new STUNClient();
+//$sc->setServerAddr("stun.ekiga.net"); 
+//$sc->setServerAddr("stun.iptel.org");
+$sc->setServerAddr("stunserver.org");
+$sc->createSocket();
+print("NAT TYPE:" . $sc->natType2String($sc->getNatType()) ."\n");
+print("MAPPED ADDRESS:" . $sc->getMappedAddr() . "\n");
+?>
